226d67036f9b2045eccccc3f8e7bef7a47632e8c
Mercedes <mercedeswagner84@gmail.com>
2022-02-08 15:20:10 +0100

    first push

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+let home = document.getElementById("navHome");
+let changeMe = document.getElementById("navChange");
+
+
+function changeLink() {
+    // console.log("test"); (funktioniert nicht)
+
+    home.classList.toggle("changed");
+}
+
+
